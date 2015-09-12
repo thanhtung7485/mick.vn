5795dd0ebaf24eb8218c05f26ca426b7d231a65f
bold9800 <bold8700@gmail.com>
2015-09-12 14:41:16 +0700

    ac

---
+++ EagleDigital/EagleDigital.Service/Services/DomainService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EagleDigital.DbFirst.Model;
+using EagleDigital.DbFirst.Repositories;
+
+namespace EagleDigital.Service.Services
+{
+    public  class DomainService : IDomainService
+    {
+
+        private readonly IEntityRepository<Domain> _domainRepository;
+        public DomainService(IEntityRepository<Domain> domainRepository)
+        {
+            this._domainRepository = domainRepository;
+        }
+
+        public IQueryable<Domain> List()
+        {
+            var domainList = _domainRepository.GetAll().OrderBy(p => p.Name);
+            return domainList;
+        }
+
+        public Domain Details(int id)
+        {
+            var domainDetails = _domainRepository.Get(id);
+            return domainDetails;
+        }
+
+        public Domain Insert(Domain domain)
+        {
+            var domainDetails = new Domain();
+            domainDetails.Name = domain.Name;
+            domainDetails.Description = domain.Description;
+
+            domainDetails = _domainRepository.InsertOnCommit(domainDetails);
+            _domainRepository.CommitChanges();
+            return domainDetails;
+        }
+
+        public Domain Update(Domain domain)
+        {
+            var domainDetails = _domainRepository.Get(domain.Id);
+            domainDetails.Name = domain.Name;
+            domainDetails.Description = domain.Description;
+            _domainRepository.CommitChanges();
+            return domainDetails;
+        }
+    }
+}
---
+++ EagleDigital/EagleDigital.Web/Models/HomeModelView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EagleDigital.DbFirst.Model;
+
+namespace EagleDigital.Web.Models
+{
+    public class HomeModelView
+    {
+        public List<Domain> ListDomain { get; set; } 
+        public List<Category> ListCategory { get; set; } 
+    }
+}
