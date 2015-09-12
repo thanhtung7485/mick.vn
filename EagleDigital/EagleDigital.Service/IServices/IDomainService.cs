3cdd9c5b7990c3546045172d10058aae9f631416
bold9800 <bold8700@gmail.com>
2015-09-12 14:38:11 +0700

    abc

---
+++ EagleDigital/EagleDigital.Service/IServices/IDomainService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EagleDigital.DbFirst.Model;
+
+namespace EagleDigital.Service.Services
+{
+    public interface IDomainService
+    {
+        IQueryable<Domain> List();
+        Domain Details(int id);
+        Domain Insert(Domain domain);
+        Domain Update(Domain domain);
+    }
+}
