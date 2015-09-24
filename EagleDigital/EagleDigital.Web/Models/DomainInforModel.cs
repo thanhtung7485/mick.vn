be0e12c02d1f768f0f6d47a974556760e9744a24
bold9800 <bold8700@gmail.com>
2015-09-24 22:35:18 +0700

    update

---
+++ EagleDigital/EagleDigital.Web/Models/DomainInforModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EagleDigital.DbFirst.Model;
+
+namespace EagleDigital.Web.Models
+{
+    public class DomainInforModel
+    {
+        public List<DomainInfor> ListDomainInfors { get; set; }
+    }
+}
