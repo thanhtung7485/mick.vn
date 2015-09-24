eafb7591aef1f1c846c126b47ea56d072bd0e0db
bold9800 <bold8700@gmail.com>
2015-09-24 22:34:48 +0700

    update

---
+++ EagleDigital/EagleDigital.Service/IServices/IDomainInforService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EagleDigital.DbFirst.Model;
+
+namespace EagleDigital.Service.IServices
+{
+    public  interface IDomainInforService
+    {
+        IQueryable<DomainInfor> List();
+        DomainInfor Details(int id);
+        DomainInfor Insert(DomainInfor domainInfor);
+        DomainInfor Update(DomainInfor domainInfor);
+    }
+}
---
+++ EagleDigital/EagleDigital.Service/IServices/ITabNameService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EagleDigital.DbFirst.Model;
+
+namespace EagleDigital.Service.IServices
+{
+    public interface ITabNameService
+    {
+        IQueryable<TabName> List();
+        TabName Details(int id);
+        TabName Insert(TabName tabName);
+        TabName Update(TabName tabName);
+    }
+}
