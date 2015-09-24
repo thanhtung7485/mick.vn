01cd51841734d6229939fabce5f7ffdb8f7e5a51
bold9800 <bold8700@gmail.com>
2015-09-24 22:35:02 +0700

    update

---
+++ EagleDigital/EagleDigital.Service/Services/DomainInforService.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EagleDigital.DbFirst.Model;
+using EagleDigital.DbFirst.Repositories;
+using EagleDigital.Service.IServices;
+
+namespace EagleDigital.Service.Services
+{
+    public class DomainInforService :IDomainInforService
+    {
+        private readonly IEntityRepository<DomainInfor> _domainInforRepository;
+
+        public DomainInforService(IEntityRepository<DomainInfor> domainInforRepository)
+        {
+            _domainInforRepository = domainInforRepository;
+        }
+            
+        public IQueryable<DomainInfor> List()
+        {
+            var domainInfor = _domainInforRepository.GetAll();
+            return domainInfor;
+        }
+
+        public DomainInfor Details(int id)
+        {
+            var domainInforDetails = _domainInforRepository.Get(id);
+            return domainInforDetails;
+        }
+
+        public DomainInfor Insert(DomainInfor domainInfor)
+        {
+            var domainDetails = new DomainInfor();
+            domainDetails.DomainId = domainInfor.DomainId;
+            domainDetails.Content = domainInfor.Content;
+            domainDetails.TabNameId = domainInfor.TabNameId;
+
+            domainDetails = _domainInforRepository.InsertOnCommit(domainDetails);
+            _domainInforRepository.CommitChanges();
+            return domainDetails;
+        }
+
+        public DomainInfor Update(DomainInfor domainInfor)
+        {
+            var domainDetails = _domainInforRepository.Get(domainInfor.Id);
+            domainDetails.DomainId = domainInfor.DomainId;
+            domainDetails.Content = domainInfor.Content;
+            domainDetails.TabNameId = domainInfor.TabNameId;
+
+            _domainInforRepository.CommitChanges();
+            return domainDetails;
+        }
+    }
+}
---
+++ EagleDigital/EagleDigital.Service/Services/TabNameService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EagleDigital.DbFirst.Model;
+using EagleDigital.DbFirst.Repositories;
+using EagleDigital.Service.IServices;
+
+namespace EagleDigital.Service.Services
+{
+    public class TabNameService : ITabNameService
+    {
+
+        private readonly IEntityRepository<TabName> _tabNameRepository;
+        public TabNameService(IEntityRepository<TabName> tabNameRepository)
+        {
+            _tabNameRepository = tabNameRepository;
+        }
+
+        public IQueryable<TabName> List()
+        {
+            var tabNameList = _tabNameRepository.GetAll().OrderBy(p => p.Name);
+            return tabNameList;
+        }
+
+        public TabName Details(int id)
+        {
+            var tabNameDetails = _tabNameRepository.Get(id);
+            return tabNameDetails;
+        }
+
+        public TabName Insert(TabName tabName)
+        {
+            var tabNameDetails = new TabName();
+            tabNameDetails.Name = tabName.Name;
+            tabNameDetails = _tabNameRepository.InsertOnCommit(tabNameDetails);
+            _tabNameRepository.CommitChanges();
+            return tabNameDetails;
+        }
+
+        public TabName Update(TabName tabName)
+        {
+            var tabNameDetails = _tabNameRepository.Get(tabName.Id);
+            tabNameDetails.Name = tabName.Name;
+            _tabNameRepository.CommitChanges();
+            return tabNameDetails;
+        }
+    }
+}
