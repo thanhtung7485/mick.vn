a5c0d3dae8e57f751cbfe0b0a2752dbc98d04a97
bold9800 <bold8700@gmail.com>
2015-10-08 22:01:28 +0700

    udpate

--- EagleDigital/EagleDigital.Web/Areas/Admin/AdminAreaRegistration.cs
+++ EagleDigital/EagleDigital.Web/Areas/Admin/AdminAreaRegistration.cs
@@ -17,9 +17,16 @@ namespace EagleDigital.Web.Areas.Admin
             context.MapRoute(
                 "Admin_default",
                 "Admin/{controller}/{action}/{id}",
-                new {controller = "Home", action = "Index", id = UrlParameter.Optional },
+                new { controller = "DomainInfor", action = "Index", id = UrlParameter.Optional },
                 new[] { "EagleDigital.Web.Areas.Admin.Controllers" }
             );
+
+           // context.MapRoute(
+           //    "Admin_defaults",
+           //    "{controller}/{action}/{id}",
+           //    new { controller = "DomainInfor", action = "Index", id = UrlParameter.Optional },
+           //    new[] { "EagleDigital.Web.Areas.Admin.Controllers" }
+           //);
         }
     }
 }
